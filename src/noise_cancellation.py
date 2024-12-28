0488f1775c9ef48b85e358ae0fb7731c10a557ae
MaheswaraKaindra <19623197@std.stei.itb.ac.id>
2024-12-28 22:02:02 +0700

    fix: noise_cancellation.py

--- src/noise_cancellation.py
+++ src/noise_cancellation.py
@@ -3,6 +3,8 @@ import scipy.io.wavfile as wav
 from scipy.linalg import svd
 import os
 
+INPUT_FILES_PATH = "../data/audio-files/"
+OUTPUT_FILES_PATH = "../data/output-files/"
 def noise_cancellation(input_file, output_file, threshold):
     # Loading audio file
     if not os.path.exists(input_file):
@@ -27,8 +29,12 @@ def noise_cancellation(input_file, output_file, threshold):
     # Applying SVD to the audio Matrix
     u, sigma, v_transposed = svd(audio_matrix, full_matrices=False)
 
+    print("U Matrix", u)
+    print("Sigma Matrix", sigma)
+    print("v_transposed Matrix", v_transposed)
+
     # Filtering out noise
-    anti_sigma = np.where(sigma <= threshold, sigma, 0)
+    anti_sigma = np.where(sigma < threshold, sigma, 0)
     filtered_sigma = sigma - anti_sigma
 
     # Reconstructing audio signal
@@ -41,7 +47,9 @@ def noise_cancellation(input_file, output_file, threshold):
     wav.write(output_file, rate, filtered_audio)
     print("Noise Cancellation Done")
 
-input_file = input("Insert .wav file path: ")
-output_file = input("Insert output file path: ")
-threshold = float(input("Insert threshold: "))
+temp_input_file = input("Insert .wav file path: ")
+input_file = INPUT_FILES_PATH + temp_input_file
+temp_input_file = input("Insert output file path: ")
+output_file = OUTPUT_FILES_PATH + temp_input_file
+threshold = float(input("Insert threshold (20 is Hardly Recommended): "))
 noise_cancellation(input_file, output_file, threshold)
