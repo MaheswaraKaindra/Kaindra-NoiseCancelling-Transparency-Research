7ced3f211d6ffc85653c199b680a14254b69d32f
MaheswaraKaindra <19623197@std.stei.itb.ac.id>
2024-12-31 03:54:06 +0700

    fix: noise_cancellation.py

--- src/noise_cancellation.py
+++ src/noise_cancellation.py
@@ -29,26 +29,13 @@ def noise_cancellation(input_file, output_file, threshold):
     chunk_size = 1024
     num_chunks = len(data) // chunk_size
     audio_matrix = np.reshape(data[:num_chunks * chunk_size], (num_chunks, chunk_size))
-    print("Audio Matrix: \n", audio_matrix)
-    print("\n")
 
     # Melakukan SVD pada matriks audio
     u, sigma, v_transposed = svd(audio_matrix, full_matrices=False)
 
-    print("U Matrix: \n", u)
-    print("\n")
-    print("Sigma Matrix: \n", sigma)
-    print("\n")
-    print("v_transposed Matrix: \n", v_transposed)
-    print("\n")
-
     # Mengeluarkan noise dari sigma
     anti_sigma = np.where(sigma < threshold, sigma, 0)
-    print("Anti Sigma: \n", anti_sigma)
-    print("\n")
     filtered_sigma = sigma - anti_sigma
-    print("Filtered Sigma: \n", filtered_sigma)
-    print("\n")
 
     # Rekonstruksi audio
     filtered_audio = np.dot(u, np.dot(np.diag(filtered_sigma), v_transposed)).flatten()
